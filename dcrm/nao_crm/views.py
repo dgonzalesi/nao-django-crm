653ff8285e6841780781e53d05c2e3cb6957f857
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-04-22 19:10:18 -0600

    Add bootstrap

--- dcrm/nao_crm/views.py
+++ dcrm/nao_crm/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
