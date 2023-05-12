e6857adf774371fc93f8ca7fd60414ba9e5daea0
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-05-12 07:32:18 -0600

    Add logout functionality.

--- dcrm/nao_crm/views.py
+++ dcrm/nao_crm/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # Create your views here.
@@ -6,13 +6,24 @@ from django.contrib import messages
 def index(request):
     if request.method == "POST":
         username = request.POST['username']
-        password = request.POST.get("password")
-        print(request.POST['username'])
-        print(request.POST)
-    return render(request, 'index.html', {})
-
-def login_user(request):
-    pass
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, ("You have been logged in!"))
+            return redirect('index')
+        else:
+            messages.success(request, "Error logging in - please try again...")
+            print(messages)
+            return redirect('index')
+    else:
+        return render(request, 'index.html', {})
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('index')
+
+def register_user(request):
+    return render(request, 'register.html', {})
+
